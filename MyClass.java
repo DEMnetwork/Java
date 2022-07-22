7d1ef173769d98ffa858969b772d614593b481f9
DEMnetwork <105674889+DEMnetwork@users.noreply.github.com>
2022-07-22 16:40:28 -0300

    ???
    ???

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=x+y;
+
+      System.out.println("Sum of x+y = " + z);
+
+        int Hello=10;
+        int test=9;
+        int HelloWorld=Hello+test; 
+        
+        System.out.println("10+9 = " + HelloWorld);
+    }
+}
